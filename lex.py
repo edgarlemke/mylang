a80b6d1ce568dfcf596694edfdcdeea158c949e4
Edgar Roberto Lemke <edgar_lemke@yahoo.com.br>
2023-01-09 12:56:56 -0300

    Work on lexer and lexer tests

---
+++ lex.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import argparse
+import re
+
+
+
+def tokenize (code):
+    """
+    Tokenize code according to rules.
+
+    Returns a list
+    """
+
+    # return variable
+    token_list = []
+
+    # rules list
+    # structure of rules is a list: ["TOKEN_CAT", "REGEXP"]
+    # TOKEN_CAT is the token category keyword
+    # REGEXP is the regular expression for matching tokens
+    rules_list = [
+        ["PAR_OPEN", "\("],
+        ["PAR_CLOSE", "\)"],
+        ["SPACE", " "],
+        ["QUOTE", "\""],
+        ["VALUE", "\w+"],
+    ]
+
+    # iter over rules list matching tokens
+    for rule in rules_list:
+        # shortcut variables
+        token_cat = rule[0]
+        regexp = rule[1]
+
+        # find all matches for regexp in code
+        matches = re.finditer(regexp, code, flags= re.UNICODE)
+        # iter over matches
+        for match in matches:
+            # extract start, end and value
+            start = match.span()[0]
+            end = match.span()[1]
+            value = match.group()
+
+            # register the info as a found token
+            token = [token_cat, start, end, value]
+            #print(token)
+            token_list.append(token)
+
+    # match VALUE tokens
+    quotes = []
+    for token in token_list:
+        # skip all non-QUOTE tokens
+        if token[0] != "QUOTE":
+            continue
+
+        # skip escaped QUOTE tokens, convert them to VALUE tokens
+        # keeping escaped \ char as VALUE
+        if code[ token[1]-1 ] == "\\" and code[ token[1]-2 ] != "\\":
+            #token[0] = "VALUE"
+            continue
+
+        quotes.append(token)
+
+    # iter over every two quotes
+    i = 0
+    while i < len(quotes):
+        quote = quotes[i]
+
+        # get initial value start and end
+        value_start = quote[2]
+        value_end = len(code)
+
+        # if there's a next QUOTE, get a new value end
+        if len(quotes) > (i+1):
+            end_quote = quotes[i+1]
+            value_end = end_quote[1]
+
+        # if start is different from end, add quoted VALUE token to list
+        if value_start != value_end:
+            value = code[value_start : value_end]
+            token = ["VALUE", value_start, value_end, value]
+            token_list.append(token)
+
+        i += 2
+
+    # remove tokens with same start and end (meant for VALUE tokens)
+    cp = token_list.copy()
+    for token in cp:
+        for token2 in cp:
+            # change only different tokens, skip non-VALUE tokens
+            if id(token) == id(token2) or token[0] != "VALUE":
+                continue
+
+            # token[0] is always "VALUE"...
+
+            # remotions for tokens of same start and end
+            if (token[1] == token2[1]) and (token[2] == token2[2]):
+                # remove duplicated VALUE tokens
+                if token2[0] == "VALUE":
+                    cp.remove(token2)
+
+                # remove SPACE, PAR_OPEN and PAR_CLOSE tokens that correspond to VALUE tokens between QUOTE tokens
+                if token2[0] in ['SPACE','PAR_OPEN','PAR_CLOSE']:
+                    cp.remove(token2)
+            
+            ## remove QUOTE tokens that correspond to scaped quotes between QUOTE tokens
+            if token2[0] == "QUOTE" and token[1] == token2[1]-1 and token[2] == token2[2]:
+               cp.remove(token2)
+    token_list = cp
+
+    # sort token_list list by start position of the token
+    token_list.sort(key=lambda x: x[1]) 
+
+
+    return token_list
+
+
+def list_print (l) :
+    """
+    Convert Python3 lists to their printable version in LISP-like format
+
+    Returns a str
+    """
+
+    # lists always start with (
+    to_print = ["("]
+
+    # iter over list, looking for other lists to convert
+    for i in l:
+        # if found a list, call this same function to convert it
+        if type(i) == list:
+            cvt = list_print(i)
+
+        # if didn't find a list, put the value between quotes for printing
+        else:
+            cvt = "\"%s\"" % str(i)
+
+        # if there's previous content in to_print list, add a space before our new content
+        if len(to_print) > 1 and to_print[len(to_print)-1] != ")" and cvt[0] != "(": 
+            to_print.append(" ")
+
+        # append the converted new content to to_print
+        to_print.append(cvt)
+
+    # lists always end with )
+    to_print.append(")")
+
+    # join the to_print list into a str
+    r = "".join(to_print)
+
+    return r
+
+if __name__ == "__main__":
+
+    # set up command line argument parsing
+    parser = argparse.ArgumentParser()
+    
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--src")
+    group.add_argument("--expr")
+
+    args = parser.parse_args()
+
+    # get the src file argument
+    src = args.src
+
+    # get the expr argument
+    expr = args.expr
+
+    # extract expr from src file
+    if src != None:
+        src = str(src)
+
+        # create a file descriptor for the src file
+        with open(src,"r") as fd:
+        
+            # read all content of the file into an variable
+            code = fd.readlines()
+            expr = "".join(code)
+
+    elif expr != None:
+        expr = str(expr)
+        
+    # generate token list from content variable
+    token_list = tokenize(expr)
+
+    print( list_print(token_list) )
---
+++ test/lex.py
@@ -0,0 +1,227 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import argparse
+from subprocess import Popen, PIPE
+from shlex import split
+import unicodedata
+
+
+def basictest (msg, expected, expr):
+    print(msg, end="")
+
+    p = Popen(split(f"/usr/bin/python3 ../lex.py --expr \"{expr}\""), stdout= PIPE, stderr= PIPE)
+    stdout, stderr = p.communicate()
+
+    stdout_dec = stdout.decode()
+    stdout_ok = (stdout_dec == expected)
+
+    if stderr != b'':
+        print("FAIL - stderr not empty: %s" % stderr)
+        exit()
+
+    if not stdout_ok:
+        print("""FAIL - stdout not correct:
+        expected:   %s
+        got:        %s""" % (expected,stdout_dec))
+        exit()
+
+    print("OK")
+
+
+def test_par_open () :
+    """
+    `(` expresssion should return only one PAR_OPEN token
+    """
+    basictest("TEST PAR_OPEN - ", "((\"PAR_OPEN\" \"0\" \"1\" \"(\"))\n", "(")
+
+
+def test_par_close () :
+    """
+    `)` expression should return only one PAR_CLOSE token
+    """
+    basictest("TEST PAR_CLOSE - ", "((\"PAR_CLOSE\" \"0\" \"1\" \")\"))\n", ")")
+
+
+def test_space () :
+    """
+    ` ` expression should return only one SPACE token
+    """
+    basictest("TEST SPACE - ", "((\"SPACE\" \"0\" \"1\" \" \"))\n", " ")
+
+
+def test_par_open_space_par_close () :
+    """
+    `( )` expression should return three tokens == PAR_OPEN, SPACE, PAR_CLOSE
+    
+    list_print() is tested for absence of spaces between lists inside a list
+    tokenize() is tested for ordering of tokens
+    """
+    basictest(
+            "TEST PAR_OPEN SPACE PAR_CLOSE - ",
+            """(("PAR_OPEN" "0" "1" "(")("SPACE" "1" "2" " ")("PAR_CLOSE" "2" "3" ")"))\n""",
+            "( )"
+    )
+
+
+def test_quote () :
+    """
+    `"` expression should return only one QUOTE token
+    """
+    expr = "\\\""
+    basictest("TEST QUOTE - ", """(("QUOTE" "0" "1" "\""))\n""", expr)
+
+def test_value () :
+    """
+    `mynametoken` expression should return only one VALUE token
+    """
+    basictest("TEST VALUE - ", """(("VALUE" "0" "11" "mynametoken"))\n""", "mynametoken")
+
+def test_abc_xyz () :
+    """
+    `(\"abc\" \"xyz\")` expression should return:
+        one PAR_OPEN token
+        one QUOTE token
+        one VALUE token
+        one QUOTE token
+        one SPACE token
+        one QUOTE token
+        one VALUE token
+        one QUOTE token
+        one PAR_CLOSE token
+    """ 
+    basictest(
+            "TEST (\"abc\" \"xyz\") - ",
+            """(("PAR_OPEN" "0" "1" "(")("QUOTE" "1" "2" "\"")("VALUE" "2" "5" "abc")("QUOTE" "5" "6" "\"")("SPACE" "6" "7" " ")("QUOTE" "7" "8" "\"")("VALUE" "8" "11" "xyz")("QUOTE" "11" "12" "\"")("PAR_CLOSE" "12" "13" ")"))\n""",
+            "(\\\"abc\\\" \\\"xyz\\\")"
+    )
+
+
+
+def _test_unicode (i) :
+    ch = chr(i)
+    cat = unicodedata.category(ch)
+
+    # characters which need escaping for the test to work
+    escape = ['\'', ' ']
+    if ch in escape:
+        expr = f"\"\{ch}\""
+    # escaping for " char
+    elif ch == "\"":
+        expr = "\\\\\\\""
+    # escaping for \ char
+    elif ch == "\\":
+        expr = "\\\\\\\\"
+    # other characters
+    else:
+        expr = f"\"{ch}\""
+
+    
+    cmd = f"""/usr/bin/python3 ../lex.py --expr "\\\"{expr}\\\"" """
+    sp = split(cmd)
+
+    try:
+        p = Popen(sp, stdout= PIPE, stderr= PIPE, encoding="utf-8")
+        stdout, stderr = p.communicate()
+
+    except Exception as e:
+        return (False, f"FAIL - char {i} {ch} - {sp} - {e}")
+
+
+    stdout_dec = stdout
+    if ch in ["\"","\\"]:
+        expected = f"""(("QUOTE" "0" "1" "\"")("VALUE" "1" "3" "\{ch}")("QUOTE" "3" "4" "\""))\n"""
+    else:
+        expected = f"""(("QUOTE" "0" "1" "\"")("VALUE" "1" "2" "{ch}")("QUOTE" "2" "3" "\""))\n"""
+
+    hex_expected = [(ord(ch),ch) for ch in expected]
+    hex_stdout = [(ord(ch),ch) for ch in stdout_dec]
+
+    if stderr != "":
+        msg = f"FAIL - char {i} {ch} - {sp} - stderr not empty: {stderr}"
+        return (False,msg)
+
+    if stdout_dec != expected:
+        msg = f"""FAIL - char {i} {ch} - {sp} - stdout not correct:
+        expected:   {expected}
+                    {hex_expected}
+        got:        {stdout_dec}
+                    {hex_stdout}"""
+        return (False,msg)
+
+    return (True,"")
+
+
+def _get_codepoints () : 
+    import sys
+
+    codepoints = []
+
+    for i in range(0,sys.maxunicode+1):
+        ch = chr(i)
+        ucat = unicodedata.category(ch)
+
+        # exclude control characters
+        vcats = [
+#                'Cc', 'Cf', 'Cn', 'Co', 'Cs',      
+                'Ll', 'Lm', 'Lo', 'Lt', 'Lu',
+                'Mc', 'Me', 'Mn',
+                'Nd', 'Nl', 'No',
+                'Pc', 'Pd', 'Pe', 'Pf', 'Pi', 'Po', 'Ps',
+                'Sc', 'Sk', 'Sm', 'So',
+                'Zl', 'Zp', 'Zs'
+        ]
+        if ucat not in vcats:
+            continue
+
+        codepoints.append(i)
+
+    return codepoints
+
+
+def test_quoted_unicode () :
+    """
+    All valid Unicode chars between QUOTE tokens should return a VALUE token
+    " char should be escaped
+    """
+    print("TEST QUOTED UNICODE - ", end="", flush= True)
+
+    from multiprocessing import Pool
+
+    codepoints = _get_codepoints()
+    with Pool() as p:
+        results = []
+
+        for i in codepoints:
+            ch = chr(i)
+
+            r = p.apply_async(_test_unicode, args=(i,))
+            results.append(r)
+        
+        for r in results:
+            ok, msg = r.get()
+            if ok is False:
+                print(msg, flush= True)
+                exit()
+
+    print("OK")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fast", action= "store_true")
+    args = parser.parse_args()
+
+    fast = args.fast
+
+
+    slow = ["test_quoted_unicode"]
+
+    tests = [t for t in globals() if t[0:5] == "test_"]
+    for t in tests:
+
+        if fast and t in slow:
+            continue
+
+        eval(f"{t}()")
