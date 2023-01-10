353fd33bc3b5c46067a068a5d0cfd08a3cd7adb5
Edgar Roberto Lemke <edgar_lemke@yahoo.com.br>
2023-01-10 01:13:17 -0300

    Changes to make first parser test in test/run.py to work. Removed test/parse.py

--- run.py
+++ run.py
@@ -7,26 +7,48 @@ import parse
 import list as list_
 
 
-def run (src, print_parse_tree= False) :
-    with open(src, "r") as fd:
-        code = fd.readlines()
-        expr = "".join(code)
-
+def run (expr, print_parse_tree= False) :
     token_list = lex.tokenize(expr) 
     parsetree = parse.parse(token_list)
 
     if print_parse_tree:
-        print( list_.list_print(parsetree) )
+        print( list_.list_print(parsetree), end="" )
         exit()
 
 
 if __name__ == "__main__":
+
+    # set up command line argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument("src")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--src")
+    group.add_argument("--expr")
+
     parser.add_argument("--print-parse-tree", action= "store_true")
+
     args = parser.parse_args()
 
-    src = str(args.src)
+    # get the src file argument
+    src = args.src
+
+    # get the expr argument
+    expr = args.expr
+
+    # extract expr from src file
+    if src != None:
+        src = str(src)
+
+        # create a file descriptor for the src file
+        with open(src,"r") as fd:
+        
+            # read all content of the file into an variable
+            code = fd.readlines()
+            expr = "".join(code)
+
+    elif expr != None:
+        expr = str(expr)
+
     print_parse_tree = args.print_parse_tree
 
-    run(src, print_parse_tree)
+    run(expr, print_parse_tree)
--- test/run.py
+++ test/run.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
 
 from subprocess import Popen, PIPE
+from shlex import split
 
 
 def test_parser_rule_0 () :
-    pass
+    print("TEST PARSER RULE 0 - ", end="", flush= True)
+
+    stdout, stderr = _popen("\\\" \\\"")
+
+    if stderr != "":
+        print(f"FAIL - stderr not empty: {stderr}", flush= True)
+        exit()
+
+    expected = """(("EXPR" (("QUOTE" "0" "1" "\"") ("VALUE" "1" "2" " ") ("QUOTE" "2" "3" "\""))))"""
+    if stdout != expected:
+        print(f"""FAIL - stdout not corrent:
+            expected:   {expected}
+            got:        {stdout}""", flush= True)
+        exit()
+
+    print("OK")
+
+
+def _popen (expr) :
+    cmd = f"/usr/bin/python3 ../run.py --expr \"{expr}\" --print-parse-tree"
+    sp = split(cmd)
+
+    p = Popen(sp, stdout= PIPE, stderr= PIPE, encoding= "utf-8")
+    return p.communicate()
 
 
 if __name__ == "__main__":
-    pass
+    tests = [t for t in globals() if t[0:5] == "test_"]
+    for t in tests:
+        eval(f"{t}()")
